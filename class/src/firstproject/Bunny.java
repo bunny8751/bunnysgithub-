b81f4371935602ee75265f387f62f1fd8fb2afd2
bunny8751 <48263899+bunny8751@users.noreply.github.com>
2019-03-05 19:13:43 -0500

    added first project

---
+++ class/src/firstproject/Bunny.java
@@ -0,0 +1,9 @@
+package firstproject;
+
+public class Bunny {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("this is my first project");
+}
+}
