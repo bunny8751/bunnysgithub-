9ebdfcfbd3f1e129416192b5d1d772f6a3052303
bunny8751 <48263899+bunny8751@users.noreply.github.com>
2019-03-05 22:41:27 -0500

    add second class project some changes occure Change-Id: Ie70cb8e199668fdb9c568d964318533eb3fdcadd

---
+++ class/src/firstproject/Secondproject.java
@@ -0,0 +1,12 @@
+package firstproject;
+
+public class Secondproject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is my second project");
+		System.out.println("hi");
+
+	}
+
+}
