414ad7b2f63ed5e7cfc3f6507391dc0eab68bd66
bunny8751 <48263899+bunny8751@users.noreply.github.com>
2019-03-10 16:24:53 -0400

    hit hub final try

---
+++ githubpractice.java
@@ -0,0 +1,14 @@
+package github;
+
+public class githubpractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("its a github practise");
+System.out.println("pull your project push your project and change");
+System.out.println("changes under script");
+System.out.println("history updates");
+System.out.println("just fuck it");
+	}
+
+}
