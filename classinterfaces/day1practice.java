2374ddf0fba576e756307326a327cc633e079a17
bunny8751 <48263899+bunny8751@users.noreply.github.com>
2019-03-10 13:12:46 -0400

    Create day1practice.java
    new project added

---
+++ classinterfaces/day1practice.java
@@ -0,0 +1,16 @@
+package classinterfaces;
+
+public class day1practice {
+
+	int x=300;
+	int y=100;
+	int z=200;
+	
+	public static void main(String[] args) {
+	
+		System.out.println("hi i m int x = 300");
+		
+
+	}
+
+}
